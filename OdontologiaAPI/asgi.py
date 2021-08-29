1f71f5cb83e0996f4b345a96d3ba081e084937da
Hernandezjose <ext_josegura@mercadolibre.com>
2021-08-29 20:16:53 -0300

    Se inicia proyecto

---
+++ OdontologiaAPI/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for OdontologiaAPI project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OdontologiaAPI.settings')
+
+application = get_asgi_application()
